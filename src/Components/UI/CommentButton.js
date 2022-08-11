cb022a3e615ad195c017aed84d89e435ac7fb79c
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-08-11 12:18:38 -0500

    cleared up comment button

--- src/Components/UI/CommentButton.js
+++ src/Components/UI/CommentButton.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { FloatingLabel, Form } from "react-bootstrap";
 
 import "../Styles/Button.css";
 import Modals from "./Modals";
 
 const CommentButton = (props) => {
-  return (
-    <Modals>
-      <FloatingLabel controlId="floatingTextarea2" label="Comments">
-        <Form.Control
-          as="textarea"
-          placeholder="Leave a comment here"
-          style={{ height: "100px" }}
-        />
-      </FloatingLabel>
-    </Modals>
-  );
+  return <Modals></Modals>;
 };
 
 export default CommentButton;
