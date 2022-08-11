7b13dd83c067195a03a273b9709a44985545fbd0
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-08-11 10:48:14 -0500

    added submit button and modal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.scss";
 import Nav from "./Components/UI/Nav";
 import Header from "./Components/Layout/Header";
 import Cards from "./Components/UI/Cards";
+import SubmitButton from "./Components/UI/SubmitButton";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <main>
         <Cards />
+        <SubmitButton />
       </main>
     </div>
   );
---
+++ src/Components/UI/SubmitButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Button } from "react-bootstrap";
+import "../Styles/Button.css";
+
+const SubmitButton = (props) => {
+  return <Button className="button">Comment</Button>;
+};
+
+export default SubmitButton;
